19cc78d71035136181bee917af37f2a958ae9553
Martin Fischer <fischerfm@siemens.com>
2025-01-05 00:14:28 +0100

    crypto-bdev: Fix formatting.

--- micropython/crypto-bdev/cryptbdev.py
+++ micropython/crypto-bdev/cryptbdev.py
@@ -24,7 +24,7 @@
 #
 # A simple Wrapper class to encrypt an existing block device
 # using AES CBS or AES ESSIV. Implementation should be compatible with Linux
-# dcrypt/LUKS
+# dcrypt/LUKE
 
 import os
 from cryptolib import aes
@@ -32,8 +32,8 @@ from hashlib import sha256
 import struct
 
 MODE_ECB = 1
-MODE_CTR = 6   # AES counter mode
-MODE_CBC = 2   # AES CBC mode
+MODE_CTR = 6  # AES counter mode
+MODE_CBC = 2  # AES CBC mode
 
 # CBC can be used together with "plain". It uses the sector number as IV.
 # see also https://gitlab.com/cryptsetup/cryptsetup/-/blob/main/FAQ.md?ref_type=heads chapter 5.13
@@ -44,7 +44,7 @@ ENCRYPT_AES_CBC = 1
 ENCRYPT_AES_ESSIV = 2
 
 
-class CryptBdev():
+class CryptBdev:
     def __init__(self, bdev, key=None, mode=ENCRYPT_AES_ESSIV):
         self.key = key if key else os.urandom(32)
         self.bdev = bdev
@@ -55,7 +55,6 @@ class CryptBdev():
         # for ESSIV mode, preallocate the IV AES generation instance
         if mode == ENCRYPT_AES_ESSIV:
             self.aes1 = aes(sha256(self.key).digest(), MODE_ECB)
-        print(f'blocksize = {self.block_size} bytes / key = {self.key}')
 
     def _get_aes_obj(self, block_num):
         iv = struct.pack("<QQ", block_num, 0)
@@ -67,17 +66,17 @@ class CryptBdev():
         # get encrypted block, decrypt it and return the required length/offset
         aes_obj = self._get_aes_obj(block_num)
         self.bdev.readblocks(block_num, self.scratch_buffer)
-        buf[:] = aes_obj.decrypt(self.scratch_buffer)[offset: offset + len(buf)]
+        buf[:] = aes_obj.decrypt(self.scratch_buffer)[offset : offset + len(buf)]
 
     def writeblocks(self, block_num, buf, offset=0):
         # decrypt block, copy over buf, then encrypt again
         aes_obj = self._get_aes_obj(block_num)
         self.bdev.readblocks(block_num, self.scratch_buffer)
-        self.scratch_buffer[offset:offset+len(buf)] = buf
+        self.scratch_buffer[offset : offset + len(buf)] = buf
         self.bdev.writeblocks(block_num, aes_obj.encrypt(self.scratch_buffer))
 
     def ioctl(self, op, arg):
-        if op == 6:   # block erase
+        if op == 6:  # block erase
             # handle page erase separately
             print(f"erasing block {arg}...")
             aes_obj = self._get_aes_obj(arg)
